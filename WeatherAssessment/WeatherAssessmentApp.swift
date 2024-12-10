99318060d7c6a26208f6477adf3fccd09b5af0d4
Cornejo, Vincent Emmanuel 43684043 <vincent-emmanuel.cornejo@ubs.com>
2024-12-10 10:05:07 +0800

    Initial Commit

---
+++ WeatherAssessment/WeatherAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAssessmentApp.swift
+//  WeatherAssessment
+//
+//  Created by Vincent Emmanuel Vizconde Cornejo on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
